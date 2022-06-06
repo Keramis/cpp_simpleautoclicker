a1045de480d98a6fe36f494f4970a76fdd6c6f77
Keramis <81401952+Keramis@users.noreply.github.com>
2022-06-05 20:43:04 -0700

    Add files via upload

--- funcs.h
+++ funcs.h
@@ -1,16 +1,13 @@
 #pragma once
-//--commented bc causes errors #include <Windows.h> //for Sleep func
-#include <iostream>
-#include <string>
-#include <vector>
+#include <iostream> //gotta have this as well
+#include <string> //you gotta have strings
+#include <vector> //simple arrays
 #include <algorithm>
+#include <conio.h> //for immediate right/left bracket.
 #include <cmath>
-#include <algorithm>
-//#include <cstdint> // for int*_t --not needed, int*_t is in iostream
-#include <conio.h> //for _getch(), which takes a key press input into [cin] w/o ENTER 
 #include <concrt.h> //for sleep function (ty kreeako)
-#include <bitset>
-#include <Windows.h>
-#include <WinUser.h>
-#include <Psapi.h>
-#include <thread>
+#include <Windows.h> //key detection
+#include <WinUser.h> //virtual keycodes
+#include <Psapi.h>	//get window name, process api
+#include <thread>	//threads to actually do smth
+#include <chrono>
