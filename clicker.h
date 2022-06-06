c63dacc6e6f4d7e19ea446554d1bc8d37b9b8d3a
Keramis <81401952+Keramis@users.noreply.github.com>
2022-06-05 20:43:11 -0700

    Add files via upload

--- clicker.h
+++ clicker.h
@@ -9,36 +9,12 @@ namespace clicker
 		SetConsoleTitleA("ScriptCat Clicker || TILDA TO EXIT");
 		std::cout << "Remember: press UP ARROW to select current active window!" << '\n';
 	}
-	bool isKeyJustReleased(DWORD virtualKey)
-	{
-		if (GetAsyncKeyState(virtualKey))
-		{
-			Sleep(1);
-			if (!GetAsyncKeyState(virtualKey))
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-	bool isKeyJustPressed(DWORD virtualKey)
-	{
-		if (!GetAsyncKeyState(virtualKey))
-		{
-			Sleep(1);
-			if (GetAsyncKeyState(virtualKey))
-			{
-				return true;
-			}
-		}
-		return false;
-	}
 	std::string GetActiveWindowTitle()
 	{
 		//tysm quicknet!
 		HWND handle = GetForegroundWindow();
-		char buf[1000]{};
-		GetWindowTextA(handle, buf, 1000);
+		char buf[500]{};
+		GetWindowTextA(handle, buf, 500);
 		std::string const window_title = buf;
 		return window_title;
 	}
@@ -48,12 +24,11 @@ namespace clicker
 		Sleep(sleepTime);
 		mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 	}
-	void leftClickWithKey(int key)
+	void rightClickMouse(int sleepTime)
 	{
-		if (GetAsyncKeyState(key))
-		{
-			leftClickMouse(1);
-		}
+		mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+		Sleep(sleepTime);
+		mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
 	}
 	SHORT GetVKFromChar(int ch)
 	{
@@ -73,16 +48,17 @@ namespace clicker
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 		std::cout << text;
 	}
+	void PrintAtCoords(int text, SHORT x, SHORT y, bool clearLine)
+	{
+		COORD c = { x, y };
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+		if (clearLine) { std::cout << clicker::dead_spaces; }
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+		std::cout << std::to_string(text);
+	}
 	int randomNum(int bottom, int top)
 	{
 		return (rand() % top + bottom);
-	}
-	int askForClickerKey()
-	{
-		std::cout << "Please enter the key you want, IN ALL CAPS, for the clicker!" << '\n';
-		char click_key{};
-		std::cin >> click_key;
-		return (CharToKey(click_key));
 	}
 	std::string get_printActiveWindow()
 	{
@@ -90,5 +66,17 @@ namespace clicker
 		std::string toprint = "Selected window: " + active_window;
 		clicker::PrintAtCoords(toprint, 0, 5, 1);
 		return active_window;
+	}
+	inline void yield(int time)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(time));
+	}
+	void fillExit()
+	{
+		for (int i = 0; i < 50; i++)
+		{
+			clicker::PrintAtCoords("Exiting...", i, i, true);
+			clicker::yield(10);
+		}
 	}
 }
