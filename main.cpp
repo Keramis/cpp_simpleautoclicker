9f5e4f5ef73a9a7d1ab5687b04be9752e42d3905
Keramis <81401952+Keramis@users.noreply.github.com>
2022-06-05 20:42:54 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,112 +1,161 @@
 #include "funcs.h"
 #include "clicker.h"
 
-using std::cout;
-using std::string;
-constexpr auto tilda_key = 0xC0;
-std::string active_window{};
+//mission: autoclicker that's not shit
+//threads: use them?
+//cpu: please no
+//use _getch() to make autoclicker go up/down (in console window)
+//use the getWindowName and up arrow to get window (this will require a thread??) [VK_UP]
+//exit the program if the [`] or [~] key is pressed (0xC0)
 
-static int g_cps = 10;
+static int g_clicks_per_second = 6;
+std::string g_windowName{};
+int g_click_key{};
+int g_click_hold = 1;
+bool g_left_active = true;
 
-namespace clickthreads
+namespace vanillaKeys
 {
-	using namespace std::chrono_literals;
-	void right_arrow() {
-		while (1)
+	extern constexpr int tilda_key = 96;
+	extern constexpr int right_bracket = 93;
+	extern constexpr int left_braket = 91;
+	extern constexpr int lowercase_v = 118;
+	extern constexpr int capital_v = 86;
+}
+
+namespace util
+{
+	void printCPS()
+	{
+		clicker::PrintAtCoords("CPS: " +
+			std::to_string(g_clicks_per_second), 0, 3, true);
+	}
+	void prePrint()
+	{
+		clicker::PrintAtCoords("----ScriptCat AutoClicker----", 0, 0, true);
+		if (!g_left_active)
 		{
-			if (clicker::isKeyJustPressed(VK_RIGHT))
+			clicker::PrintAtCoords("----CPS-[RIGHT ACTIVE]----", 0, 2, true);
+		}
+		else
+		{
+			clicker::PrintAtCoords("----CPS-[LEFT ACTIVE]----", 0, 2, true);
+		}
+		util::printCPS();
+		clicker::PrintAtCoords("----SELECTED WINDOW----", 0, 5, true);
+		//selected window at line 6
+
+		clicker::PrintAtCoords("----CLICK HOLD KEY (CAN'T BE LOWERCASE)----", 0, 9, true);
+		//selected click key at line 10
+	}
+	void upDownCPS()
+	{
+		char a = _getch();
+		Sleep(10);
+		if (!a == 0) {
+			const int vanilla = clicker::CharToKey(a);
+			switch (vanilla)
 			{
-				++g_cps;
-				clicker::PrintAtCoords("CPS: " + std::to_string(g_cps), 0, 8, true);
+			case vanillaKeys::right_bracket:
+				g_clicks_per_second++;
+				util::printCPS();
+				break;
+
+			case vanillaKeys::left_braket:
+				if (g_clicks_per_second > 1)
+				{
+					g_clicks_per_second--;
+					util::printCPS();
+				}
+				break;
+			case vanillaKeys::lowercase_v:
+			case vanillaKeys::capital_v:
+				g_left_active = !g_left_active;
+				util::prePrint();
+				break;
+			default:
+				g_click_key = vanilla;
+				std::string to_print = std::format("Click key: {} {} {}", a, " || ", vanilla);
+				clicker::PrintAtCoords(to_print, 0, 10, true);
+				break;
 			}
 		}
 	}
-	void left_arrow()
+	void exitDetection()
 	{
 		while (1)
 		{
-			if (clicker::isKeyJustPressed(VK_LEFT))
+			if (GetAsyncKeyState(0xC0)) //0xC0 is tilda key
 			{
-				--g_cps;
-				clicker::PrintAtCoords("CPS: " + std::to_string(g_cps), 0, 8, true);
+				clicker::fillExit();
+				exit(712);
 			}
+			clicker::yield(400);
 		}
 	}
-	void up_arrow()
-	{
-		while (1) {
-			if (clicker::isKeyJustPressed(VK_UP))
-			{
-				active_window = clicker::get_printActiveWindow();
-			}
-		}
-	}
-	void click(int clickKey)
+	void upArrowWindow()
 	{
 		while (1)
 		{
-			if (clicker::GetActiveWindowTitle() == active_window)
+			clicker::yield(100);
+			if (GetAsyncKeyState(VK_UP))
 			{
-				clicker::leftClickWithKey(clickKey);
-				Sleep(980 / g_cps); //dividing by 980 to compensate for exe slowness, this works best.
+				clicker::PrintAtCoords("Updating....", 0, 6, true);
+				clicker::yield(200);
+				g_windowName = clicker::GetActiveWindowTitle();
+				clicker::PrintAtCoords(g_windowName, 0, 6, true);
 			}
+			else if (GetAsyncKeyState(VK_DOWN))
+			{
+				clicker::PrintAtCoords("Updating....", 0, 6, true);
+				clicker::yield(200);
+				g_windowName = "NONE";
+				clicker::PrintAtCoords(g_windowName, 0, 6, true);
+			}
+
 		}
 	}
-	void endEverything()
+	void clickKeyDetection()
 	{
 		while (1)
 		{
-			if (clicker::isKeyJustPressed(tilda_key))
+			if (GetAsyncKeyState(g_click_key) && (clicker::GetActiveWindowTitle() == g_windowName || g_windowName == "NONE"))
 			{
-				exit(1);
+				if (g_left_active)
+				{
+					clicker::leftClickMouse(g_click_hold);
+				}
+				else
+				{
+					clicker::rightClickMouse(g_click_hold);
+				}
 			}
+			clicker::yield(999 / g_clicks_per_second); //800 instead of 1000 to compensate
 		}
 	}
 }
 
-//thank you Sai for telling me to do thread.detach!
-
 int main()
 {
+	util::prePrint(); //pre-print the text
 
-	clicker::startup();
-	int clickKey = clicker::askForClickerKey();
+	//thread for [`] detection (EXIT)
+	std::thread exit_detection(util::exitDetection);
+	exit_detection.detach();
 
-	//starting threads
+	//thread for detecting up arrow key
+	std::thread up_arrow(util::upArrowWindow);
+	up_arrow.detach();
 
-	//right arrow detection (more CPS)
-	std::thread right_detection(clickthreads::right_arrow);
-	right_detection.detach();
+	//thread for detecting the click key.
+	std::thread click_detection(util::clickKeyDetection);
+	click_detection.detach();
 
-	//left arrow detection (less CPS)
-	std::thread left_detection(clickthreads::left_arrow);
-	left_detection.detach();
-
-	//up arrow detection (window selection)
-	std::thread up_detection(clickthreads::up_arrow);
-	up_detection.detach();
-
-	//actual click thread
-	std::thread click_mouse(clickthreads::click, clickKey);
-	click_mouse.detach();
-
-	//tilda key detection thread
-	std::thread tilda_detection(clickthreads::endEverything);
-	tilda_detection.detach();
-
-	//pre-printing here:
-	clicker::PrintAtCoords("-----ACTIVE WINDOW-----", 0, 4, false);
-	clicker::PrintAtCoords("-----CPS-----", 0, 7, false);
-	clicker::PrintAtCoords("CPS: " + std::to_string(g_cps), 0, 8, false);
-
-	//this keeps the program from just returning 404.
-	//the actual tilda key detection is in a thread.
-	//probably a dogshit way of doing so but idc
-	while (!clicker::isKeyJustPressed(tilda_key))
+	while (1)
 	{
+		//this is for the `[` and `]` detection, up/down on the CPS.
+		//also for 'v' and 'V' detection, for right and left-clicking.
+		util::upDownCPS();
 		Sleep(1);
 	}
-
-	//return 404 if we somehow got here. this is very wrong if we did so.
-	return 404;
 }
